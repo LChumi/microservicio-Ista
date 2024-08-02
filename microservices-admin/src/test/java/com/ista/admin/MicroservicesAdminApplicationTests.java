526b94c71fd4d31ee8839bf5116b3115e0df97ec
LChumi <112025391+LChumi@users.noreply.github.com>
2024-08-01 20:41:38 -0500

    Inicializacion del Proyecto

---
+++ microservices-admin/src/test/java/com/ista/admin/MicroservicesAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
