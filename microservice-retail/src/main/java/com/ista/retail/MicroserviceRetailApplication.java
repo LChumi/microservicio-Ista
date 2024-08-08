e57cf7570455cdf79b67a3f7dd18a084dc51bc0a
Luis Chumi <112025391+LChumi@users.noreply.github.com>
2024-08-08 15:58:21 -0500

    feat(adding swagger)

--- microservice-retail/src/main/java/com/ista/retail/MicroserviceRetailApplication.java
+++ microservice-retail/src/main/java/com/ista/retail/MicroserviceRetailApplication.java
@@ -1,11 +1,14 @@
 package com.ista.retail;
 
+import com.ista.retail.config.OpenAPIConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Import;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@Import(OpenAPIConfiguration.class)
 public class MicroserviceRetailApplication {
 
 	public static void main(String[] args) {
